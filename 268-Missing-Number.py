50e029807ccce6d483f38399808c8bae82667aba
BHUVI-SHIP-IT <bhuvaneswar981@gmail.com>
2024-11-06 19:00:52 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 59.59%] - LeetPush

---
+++ 268-Missing-Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        expected_sum=n*(n+1)//2
+        actual_sum=sum(nums)
+        missing=expected_sum-actual_sum
+        return missing
+        
