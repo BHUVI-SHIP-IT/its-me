33552f921e1b2f071a22cfbc9ff43567f9221483
BHUVI-SHIP-IT <bhuvaneswar981@gmail.com>
2024-12-02 10:16:33 +0530

    [3330] [Time Beats: 34.82%] [Memory Beats: 15.13%] - LeetPush

---
+++ 3330-Find-the-Original-Typed-String-I.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count=1;
+        for(int i=1;i<word.size();++i){
+            if(word[i]==word[i-1]){
+                ++count;
+            }
+        }
+        return count;
+    }
+};
