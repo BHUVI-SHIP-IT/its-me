055b8db76e26de07151b1d00948583d67de927ca
BHUVI-SHIP-IT <bhuvaneswar981@gmail.com>
2024-12-02 13:57:31 +0530

    [917] [Time Beats: 100.00%] [Memory Beats: 5.44%] - LeetPush

---
+++ 917-Reverse-Only-Letters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left=0,right=s.size()-1;
+        while(left<right){
+            if(!isalpha(s[left])){
+                left++;
+            }
+            else if(!isalpha(s[right])){
+                right--;
+
+            }
+            else{
+                swap(s[left],s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+        
+    }
+};
