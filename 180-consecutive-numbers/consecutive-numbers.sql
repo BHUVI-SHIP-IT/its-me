e5fd904e2ff734456f5a27a2c1a75b499065a98d
BHUVI-SHIP-IT <bhuvaneswar981@gmail.com>
2025-04-29 19:20:07 +0530

    Time: 613 ms (54.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,9 @@
+with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
+
+)
+
+select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
