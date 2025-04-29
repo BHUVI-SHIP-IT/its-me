fab18a300ff88230d6798da14917d110f2a5b443
BHUVI-SHIP-IT <bhuvaneswar981@gmail.com>
2025-04-29 19:05:00 +0530

    Time: 384 ms (62.66%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT 
+    query_name,
+    ROUND(AVG(CAST(rating AS DECIMAL) / position), 2) AS quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
