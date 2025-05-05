192395ffed7a8dadb56550d60619a8eabbf59981
BHUVI-SHIP-IT <bhuvaneswar981@gmail.com>
2025-05-05 19:20:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (35.55%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numTilings(int n) {
+        const int mod=1e9+7;
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        vector<long long>dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;++i)
+        {
+            dp[i]=(2*dp[i-1]%mod + dp[i-3])%mod;
+
+        }
+        return dp[n];
+    }
+};
